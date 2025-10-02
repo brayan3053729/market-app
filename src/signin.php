c7e49283e55d253ecef2ab268767fbbcfdc1b58c
brayancastro <david3053729@gmail.com>
2025-10-02 11:41:55 -0500

    validasion de main

--- src/signin.php
+++ src/signin.php
@@ -3,8 +3,11 @@
     require('../config/database.php');
 
     //step2.get  form-data
-    $e_mail = $_POST['email'];
-    $p_wd = $_POST['password'];
+    $e_mail =trim($_POST['email']);
+    $p_wd = $_POST['passwd'];
+
+    //$enc_pass = password_hash($p_wd,PASSWORD_DEFAULT);
+    $enc_pass = md5($p_wd);
 
     //Step 3. query
     $sql_check_user = "
@@ -15,12 +18,13 @@
         users u
     where 
         u.email = '$e_mail' and
-        u.password = '$p_wd'
+        u.password = '$enc_pass'
     limit 1
     ";
      $res_check = pg_query($conn, $sql_check_user);
       if (pg_num_rows($res_check) > 0){
-       echo "user exists. go to mail page !!!!";
+       //echo "user exists. go to mail page !!!!";
+        header ('refresh:0;url=main.php'); 
       } else {
             echo "verify data";
       }
